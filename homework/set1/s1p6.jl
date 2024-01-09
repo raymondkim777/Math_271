2983dde601085c2780803982bf6609cc9402f819
Raymond Kim :D <72332957+raymondkim777@users.noreply.github.com>
2024-01-09 19:02:07 +0000

    problem 6-d

--- homework/set1/s1p6.jl
+++ homework/set1/s1p6.jl
@@ -15,17 +15,10 @@ diet = Model(GLPK.Optimizer)
 
 # Adding constraints
 
-@constraint(diet, constraint1, 7pb + 7wmlk + 12oats + 26beef >= 160)
-@constraint(diet, constraint2, 16pb + 4wmlk + 6oats + 12beef <= 80)
-# @constraint(diet, constraint3, 2pb + 9oats <= 25) 
-@constraint(diet, constraint4, 3pb + 6wmlk <= 40)
-
-
-#=
 @constraint(diet, constraint1, 7pb + 8wmlk + 12oats + 24.19beef >= 160)
-@constraint(diet, constraint2, 16pb + 8wmlk + 6oats + 5.6beef <= 80)
+@constraint(diet, constraint2, 16pb + 8wmlk + 6oats + 5.65beef <= 80)
+# @constraint(diet, constraint3, 2pb + 9oats <= 25) 
 @constraint(diet, constraint4, 3pb + 11wmlk <= 40)
-=#
 
 # Printing the prepared optimization model
 print(diet)
@@ -40,7 +33,7 @@ println("pb = ", value(pb))
 println("wmlk = ", value(wmlk))
 println("oats = ", value(oats))
 println("beef = ", value(beef))
-println("\nprotein: ", value(7pb + 7wmlk + 12oats + 26beef))
-println("fat: ", value(16pb + 4wmlk + 6oats + 12beef))
+println("\nprotein: ", value(7pb + 8wmlk + 12oats + 24.19beef))
+println("fat: ", value(16pb + 8wmlk + 6oats + 5.65beef))
 println("fiber: ", value(2pb + wmlk + 9oats))
-println("sugar: ", value(3pb + 6wmlk))
+println("sugar: ", value(3pb + 11wmlk))
