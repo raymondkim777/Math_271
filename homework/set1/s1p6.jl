0ea1170d6a479cb6ec3b711a215c9289411f552c
Raymond Kim :D <72332957+raymondkim777@users.noreply.github.com>
2024-01-09 02:10:45 +0000

    p6

--- homework/set1/s1p6.jl
+++ homework/set1/s1p6.jl
@@ -5,7 +5,7 @@ using JuMP, GLPK
 diet = Model(GLPK.Optimizer)
 
 # Declaring variables
-@variable(diet, pb >= 0)
+@variable(diet, pb >= 1)
 @variable(diet, wmlk >= 0)
 @variable(diet, oats >= 0)
 @variable(diet, beef >= 0)
@@ -14,10 +14,18 @@ diet = Model(GLPK.Optimizer)
 @objective(diet, Min, 0.80pb + 0.20wmlk + 0.40oats + beef)
 
 # Adding constraints
-@constraint(diet, constraint1, 16pb + 8wmlk + 6oats + 12beef <= 80)
-@constraint(diet, constraint2, 3pb + 11wmlk <= 40)
-@constraint(diet, constraint3, 7pb + 8wmlk + 12oats + 26beef >= 160)
 
+@constraint(diet, constraint1, 7pb + 7wmlk + 12oats + 26beef >= 160)
+@constraint(diet, constraint2, 16pb + 4wmlk + 6oats + 12beef <= 80)
+# @constraint(diet, constraint3, 2pb + 9oats <= 25) 
+@constraint(diet, constraint4, 3pb + 6wmlk <= 40)
+
+
+#=
+@constraint(diet, constraint1, 7pb + 8wmlk + 12oats + 24.19beef >= 160)
+@constraint(diet, constraint2, 16pb + 8wmlk + 6oats + 5.6beef <= 80)
+@constraint(diet, constraint4, 3pb + 11wmlk <= 40)
+=#
 
 # Printing the prepared optimization model
 print(diet)
@@ -32,3 +40,7 @@ println("pb = ", value(pb))
 println("wmlk = ", value(wmlk))
 println("oats = ", value(oats))
 println("beef = ", value(beef))
+println("\nprotein: ", value(7pb + 7wmlk + 12oats + 26beef))
+println("fat: ", value(16pb + 4wmlk + 6oats + 12beef))
+println("fiber: ", value(2pb + wmlk + 9oats))
+println("sugar: ", value(3pb + 6wmlk))
