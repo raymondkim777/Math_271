3b4fc408bb0f892c3f646ac91c4cdef2ec136615
Raymond Kim :D <72332957+raymondkim777@users.noreply.github.com>
2024-02-07 19:28:21 +0000

    dq5.jl revise

--- homework/group/dq5.jl
+++ homework/group/dq5.jl
@@ -3,25 +3,41 @@ using LinearAlgebra
 A = [-2 1 1 0 0 ; 1 -2 0 1 0; 1 1 0 0 1]
 b = [4, 2, 10]
 c = [2, 3, 0, 0, 0]
-B = [1, 2, 4]
+B = [3, 4, 5]
 
 # check B is basis
 if size(B)[1] > size(A)[2]
     print("LP is infeasible")
-if det(A[:, B]) == 0
+elseif det(A[:, B]) == 0
     print("B is not a basis")
 
 else
     while true
-    n = size(A)[2]
-    Q = -inv(A[:, B]) * A[:, setdiff(1:n, B)]
-    p = inv(A[:, B]) * b
-    r = c[setdiff(1:n, B)] - transpose(transpose(c[B]) * inv(A[:, B]) * A[:, setdiff(1:n, B)])
-    z0 = transpose(c[B]) * inv(A[:, B]) * b
-    
-    println("p = ", p)
-    println("Q = ", Q)
-    println("z0 = ", z0)
-    println("r = ", r)
+        n = size(A)[2]
+        Q = -inv(A[:, B]) * A[:, setdiff(1:n, B)]
+        p = inv(A[:, B]) * b
+        r = c[setdiff(1:n, B)] - transpose(transpose(c[B]) * inv(A[:, B]) * A[:, setdiff(1:n, B)])
+        z0 = transpose(c[B]) * inv(A[:, B]) * b
+        
+        println("p = ", p)
+        println("Q = ", Q)
+        println("z0 = ", z0)
+        println("r = ", r)
 
+        println("\nCurrent basis: ", B)
+        print("Entering variable index: ")
+        x_n = parse(Int64, readline())  
+        print("Exiting variable index: ")
+        x_b = parse(Int64, readline()) 
+
+        if size(findall(x->x == x_n, setdiff(1:n, B)))[1] == 0 || size(findall(x->x == x_b, B))[1] == 0
+            println("Erronious input for x_n or x_b")
+            exit()
+        end
+        println()
+
+        deleteat!(B, findall(x->x == x_b, B)[1])
+        push!(B, x_n)
+        sort!(B)
+    end    
 end
