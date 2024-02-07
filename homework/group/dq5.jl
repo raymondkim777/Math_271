9a215f86e4ae09e7ba95ab3109cc0ab996531d96
Raymond Kim :D <72332957+raymondkim777@users.noreply.github.com>
2024-02-07 20:29:05 +0000

    dq5.jl update

--- homework/group/dq5.jl
+++ homework/group/dq5.jl
@@ -13,29 +13,34 @@ elseif det(A[:, B]) == 0
 
 else
     while true
+        # calculating tableau
         n = size(A)[2]
         Q = -inv(A[:, B]) * A[:, setdiff(1:n, B)]
         p = inv(A[:, B]) * b
         r = c[setdiff(1:n, B)] - transpose(transpose(c[B]) * inv(A[:, B]) * A[:, setdiff(1:n, B)])
         z0 = transpose(c[B]) * inv(A[:, B]) * b
         
+        # printing tableau
         println("p = ", p)
         println("Q = ", Q)
         println("z0 = ", z0)
         println("r = ", r)
 
+        # pivoting
         println("\nCurrent basis: ", B)
         print("Entering variable index: ")
         x_n = parse(Int64, readline())  
         print("Exiting variable index: ")
         x_b = parse(Int64, readline()) 
 
+        # check for erronious input
         if size(findall(x->x == x_n, setdiff(1:n, B)))[1] == 0 || size(findall(x->x == x_b, B))[1] == 0
             println("Erronious input for x_n or x_b")
             exit()
         end
         println()
 
+        # modify basis
         deleteat!(B, findall(x->x == x_b, B)[1])
         push!(B, x_n)
         sort!(B)
