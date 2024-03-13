ef486391d1e120421f2a9665e9dd08328ffc093d
Raymond Kim :D <raymondkim123456789@gmail.com>
2024-03-13 14:03:52 -0500

    examples, updates

--- final/example.jl
+++ final/example.jl
@@ -16,13 +16,13 @@ using JuMP, GLPK
 m = Model(GLPK.Optimizer)
 
 # Input parameters/data
-t_max = 50                                      # DATA: 24 hours, 15 minute intervals
-delay_padding = 0                               # DATA: 2 hour delays allowed
+t_max = 96                                      # DATA: 24 hours, 15 minute intervals
+delay_padding = 8                               # DATA: 2 hour delays allowed
 
 F = [n for n=1:3]                               # array of flight numbers
 K = [2n - 1 for n=1:4]                          # array of airport numbers --> odd
 J = vcat(K, [2n for n=1:11])                    # array of sector numbers --> even
-C = Pair{Float64}[]                             # array of (f1, f2) cont. pairs
+C = Pair{Int64, Int64}[]                        # array of (f1, f2) cont. pairs
 
 P = Vector{Vector{Int64}}()                     # array of flight path arrays P[f][i] = Int64
 N = Vector{Int64}()                             # array of sector cnt in f N[f] = Int64
@@ -47,15 +47,37 @@ T = [i for i = 1:t_max]
 
 
 # Manual Entry of Data
-push!(P, [1, 2, 6, 8, 10, 7])  # first & last are airports (odd), not sectors (even)
-push!(P, [3, 12, 10, 8, 4, 5])
+push!(C, 2 => 3)
+push!(P, [1, 4, 8, 10, 14, 20, 22, 5])  # first & last are airports (odd), not sectors (even)
+push!(P, [1, 4, 2, 6, 12, 3])
+push!(P, [3, 12, 14, 16, 18, 7])
 for f in F
     push!(N, size(P[f])[1])
 end
-sect_times = Dict(1=>1, 3=>1, 5=>1, 7=>1, 2=>4, 4=>3, 6=>3, 8=>3, 10=>5, 12=>2)
-push!(l, sect_times)
-push!(l, sect_times)
+sect_times = Dict(
+    # Airports
+    1=>1, 
+    3=>1, 
+    5=>1, 
+    7=>1, 
+    # Sectors
+    2=>1, 
+    4=>1, 
+    6=>1, 
+    8=>1, 
+    10=>1, 
+    12=>1, 
+    14=>2, 
+    16=>2, 
+    18=>1, 
+    20=>1, 
+    22=>1 
+)
+for f in F
+    push!(l, sect_times)
+end
 
+# Capacities
 for k in K
     D[k] = [1 for i = 1:t_max]
     A[k] = [1 for i = 1:t_max]
@@ -64,17 +86,28 @@ for j in J
     S[j] = [1 for i = 1:t_max]
 end
 
-push!(d, 10)
-push!(d, 15)
-push!(r, 35)
-push!(r, 38)
-push!(s, 0)
-push!(s, 0)
+# Schedule
+push!(d, 40)
+push!(d, 32)
+push!(d, 42)
 
-for f in F
-    push!(c_g, 5)
-    push!(c_a, 17)
-end
+push!(r, 49)
+push!(r, 38)
+push!(r, 50)
+
+push!(s, 0)
+push!(s, 0)
+push!(s, 4)
+
+# Costs
+push!(c_g, 100)
+push!(c_a, 200)
+
+push!(c_g, 250)
+push!(c_a, 300)
+
+push!(c_g, 250)
+push!(c_a, 300)
 
 for f in F
     temp1 = Dict{Int64, Int64}()
@@ -118,7 +151,7 @@ padding = delay_padding + maximum([maximum(values(l[f])) for f in F])
 
 # additional constraints (planes have to takeoff & land)
 @constraint(m, c0_0[f in F], w[f, P[f][1], d[f] - 1] == 0)
-@constraint(m, c0_1[f in F], w[f, P[f][N[f]], r[f]] == 1)
+@constraint(m, c0_1[f in F], w[f, P[f][N[f]], r[f] + delay_padding] == 1)
 
 # given model constraints
 @constraint(m, c1[k in K, t in T], sum(vcat([0], [
@@ -164,19 +197,33 @@ close(file1)
 
 # Printing Flight Paths to File
 file2 = open("results.lp", "w")
-path = Vector{Dict{Int64, Int64}}()  # path[f][sec] = time
+path1 = Vector{Dict{Int64, Int64}}()    # path1[f][sec] = time period
+path2 = Vector{Dict{Int64, String}}()   # path2[f][sec] = actual time
 
 for f in F
-    temp = Dict{Int64, Int64}()
+    temp1 = Dict{Int64, Int64}()
+    temp2 = Dict{Int64, String}()
     for sec in P[f]
         for t in T_all[f][sec]
             if value(w[f, sec, t]) == 1
-                temp[sec] = t
+                temp1[sec] = t
+                hr = div(t, 4)
+                mi = (t % 4) * 15
+                ap = "AM"
+                if hr - 12 >= 0
+                    if hr - 12 > 0
+                        hr -= 12
+                    end
+                    ap = "PM"
+                end
+                my_str = ("0" ^ (2 - length(string(hr)))) * string(hr) * ":" * ("0" ^ (2 - length(string(mi)))) * string(mi) * " " * ap
+                temp2[sec] = my_str
                 break
             end
         end
     end
-    push!(path, temp)
+    push!(path1, temp1)
+    push!(path2, temp2)
 end
 
 println(file2, "Optimization Results:\n")
@@ -189,7 +236,7 @@ for f in F
         else
             print(file2, "Sector ")
         end
-        println(file2, sec, ": t = ", path[f][sec])
+        println(file2, sec, ": \t t = ", path1[f][sec], " --> ", path2[f][sec])
     end
     println(file2)
 end
